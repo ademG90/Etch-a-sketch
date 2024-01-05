93dfaa04cf9777981e08deaa187f836bd56092e0
Adem Kedir <ademkedir724@gmail.com>
2024-01-06 01:46:33 +0300

    fix bug, add clear button
    - fix a bug which cause rainbow button not to work with losing of
     slowly darking effect.
     - add clear button that clear canvas from any droplet of color.

--- index.js
+++ index.js
@@ -13,40 +13,27 @@ function random(number) {
   return Math.floor(Math.random() * number + 1);
 }
 
-function randomColorChanger(color) {
-  // console.log("randomcolorchange: ", color);
+function randomColorChanger() {
   const innerDiv = document.querySelectorAll(".div");
   innerDiv.forEach((div) => {
     div.addEventListener("pointerenter", (event) => {
-      if (color == "rainbow") {
-        // console.log("@@@randomcolorchange: ", color);
-        if (event.target.style.backgroundColor == "") {
-          event.target.style.backgroundColor = `rgb(${random(255)},${random(
-            255
-          )},${random(255)})`;
-
-          console.log("111111randomcolorchange: ", color);
-        } else {
-          console.log("2222randomcolorchange: ", color);
-          const colors = event.target.style.backgroundColor;
-          [col1, col2, col3] = colors
-            .slice(colors.indexOf("(") + 1, colors.indexOf(")"))
-            .split(",");
-          event.target.style.backgroundColor = `rgb(
-                  ${Math.floor(col1 - col1 * 0.1)},
-                  ${Math.floor(col2 - col2 * 0.1)},
-                  ${Math.floor(col3 - col3 * 0.1)})`;
-        }
-      } else if (color != "rainbow") {
-        console.log("333333randomcolorchange: ", color);
-        event.target.style.backgroundColor = `${color}`;
-      }
+      event.target.style.backgroundColor = `rgb(${random(255)},${random(
+        255
+      )},${random(255)})`;
     });
   });
 }
 
+function customColorChange(color) {
+  const innerDiv = document.querySelectorAll(".div");
+  innerDiv.forEach((div) => {
+    div.addEventListener("pointerenter", (event) => {
+      event.target.style.backgroundColor = color;
+    });
+  });
+}
 // let add functionality to range
-let DEFAULT_COLOR = "blue";
+let DEFAULT_COLOR = "rgb(255,0,0)";
 
 const slider = document.querySelector("#slider");
 const output = document.querySelector("output");
@@ -65,17 +52,25 @@ slider.addEventListener("input", () => {
 
 const selectColor = document.querySelector(".select-color");
 selectColor.addEventListener("click", (event) => {
-  console.log("*************", event.target.value);
   DEFAULT_COLOR = event.target.value;
 
+  if (DEFAULT_COLOR == "rainbow") {
+    randomColorChanger();
+    console.log("rainbow");
+  } else {
+    console.log(DEFAULT_COLOR);
+    customColorChange(DEFAULT_COLOR);
+  }
+});
+
+drawGrid(16);
+customColorChange(DEFAULT_COLOR);
+
+const clearBtn = document.querySelector(".clear-btn");
+clearBtn.addEventListener("click", () => {
   const innerDiv = document.querySelectorAll(".div");
   innerDiv.forEach((div) => {
     // container.removeChild(div);
     div.style.backgroundColor = "#fff";
   });
-
-  randomColorChanger(DEFAULT_COLOR);
 });
-
-drawGrid(16);
-randomColorChanger(DEFAULT_COLOR);
