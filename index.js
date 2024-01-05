44d86c61e9ecbd952d91bc4105beaec6ccefeb12
Adem Kedir <ademkedir724@gmail.com>
2024-01-06 00:54:00 +0300

    can take different color used in drawing.
    - it can take all button as color input..
    - it's working for all color button except rainbow and input.
    - it is not working at all for rainbow.
    - it work for color input but it need selected twice.

--- index.js
+++ index.js
@@ -9,46 +9,73 @@ function drawGrid(grid) {
   }
 }
 
-const gridChangeBtn = document.querySelector(".gridChange");
-gridChangeBtn.addEventListener("click", (event) => {
-  let grid = Number(prompt("Enter the grid number (less than 100)"));
-  while (grid > 100 || grid < 1 || !Number.isInteger(grid)) {
-    grid = Number(prompt("Please enter the grid number (less than 100)"));
-  }
+function random(number) {
+  return Math.floor(Math.random() * number + 1);
+}
+
+function randomColorChanger(color) {
+  // console.log("randomcolorchange: ", color);
+  const innerDiv = document.querySelectorAll(".div");
+  innerDiv.forEach((div) => {
+    div.addEventListener("pointerenter", (event) => {
+      if (color == "rainbow") {
+        // console.log("@@@randomcolorchange: ", color);
+        if (event.target.style.backgroundColor == "") {
+          event.target.style.backgroundColor = `rgb(${random(255)},${random(
+            255
+          )},${random(255)})`;
+
+          console.log("111111randomcolorchange: ", color);
+        } else {
+          console.log("2222randomcolorchange: ", color);
+          const colors = event.target.style.backgroundColor;
+          [col1, col2, col3] = colors
+            .slice(colors.indexOf("(") + 1, colors.indexOf(")"))
+            .split(",");
+          event.target.style.backgroundColor = `rgb(
+                  ${Math.floor(col1 - col1 * 0.1)},
+                  ${Math.floor(col2 - col2 * 0.1)},
+                  ${Math.floor(col3 - col3 * 0.1)})`;
+        }
+      } else if (color != "rainbow") {
+        console.log("333333randomcolorchange: ", color);
+        event.target.style.backgroundColor = `${color}`;
+      }
+    });
+  });
+}
+
+// let add functionality to range
+let DEFAULT_COLOR = "blue";
+
+const slider = document.querySelector("#slider");
+const output = document.querySelector("output");
+output.textContent = `  ${slider.value}`;
+slider.addEventListener("input", () => {
+  output.textContent = `  ${slider.value}`;
+
   const innerDiv = document.querySelectorAll(".div");
   innerDiv.forEach((div) => {
     container.removeChild(div);
   });
 
-  drawGrid(grid);
-  colorChanger();
+  drawGrid(output.textContent);
+  randomColorChanger(DEFAULT_COLOR);
 });
 
-function random(number) {
-  return Math.floor(Math.random() * number + 1);
-}
+const selectColor = document.querySelector(".select-color");
+selectColor.addEventListener("click", (event) => {
+  console.log("*************", event.target.value);
+  DEFAULT_COLOR = event.target.value;
 
-drawGrid(16);
-colorChanger();
-
-function colorChanger() {
   const innerDiv = document.querySelectorAll(".div");
   innerDiv.forEach((div) => {
-    div.addEventListener("pointerenter", (event) => {
-      if (event.target.style.backgroundColor == "") {
-        event.target.style.backgroundColor = `rgb(${random(255)},${random(
-          255
-        )},${random(255)})`;
-      } else {
-        const colors = event.target.style.backgroundColor;
-        [col1, col2, col3] = colors
-          .slice(colors.indexOf("(") + 1, colors.indexOf(")"))
-          .split(",");
-        event.target.style.backgroundColor = `rgb(
-                ${Math.floor(col1 - col1 * 0.1)},
-                ${Math.floor(col2 - col2 * 0.1)},
-                ${Math.floor(col3 - col3 * 0.1)})`;
-      }
-    });
+    // container.removeChild(div);
+    div.style.backgroundColor = "#fff";
   });
-}
+
+  randomColorChanger(DEFAULT_COLOR);
+});
+
+drawGrid(16);
+randomColorChanger(DEFAULT_COLOR);
